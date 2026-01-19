1711ca8c610dfa66e5845ee7e8f4b4e911e53f26
Antonio <antonioesposito49374@gmail.com>
2026-01-19 13:35:08 +0100

    add right arrow button

---
+++ FixTheGrocery/FixTheGrocery/AddSavingGoalView.swift
@@ -0,0 +1,18 @@
+//
+//  AddSavingGoalView.swift
+//  FixTheGrocery
+//
+//  Created by Antonio Esposito on 16/01/26.
+//
+
+import SwiftUI
+
+struct AddSavingGoalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddSavingGoalView()
+}
---
+++ FixTheGrocery/FixTheGrocery/Models/SavingGoal.swift
@@ -0,0 +1,8 @@
+//
+//  SavingGoal.swift
+//  FixTheGrocery
+//
+//  Created by Antonio Esposito on 16/01/26.
+//
+
+import Foundation
